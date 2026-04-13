0ac5bf6518614bc57291a2c9bb073c5fdefa52e1
JS SHREYAS <shreyas12js@gmail.com>
2026-04-13 22:33:28 +0530

    Update main.py

--- 06_gross_pay_with_overtime/main.py
+++ 06_gross_pay_with_overtime/main.py
@@ -1,1 +1,11 @@
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
 
+if hours <= 40:
+    pay = hours * rate
+else:
+    overtime = hours - 40
+    pay = (40 * rate) + (overtime * rate * 1.5)
+    final_pay = round(pay , 2)
+
+print(f"Pay: {final_pay}")
