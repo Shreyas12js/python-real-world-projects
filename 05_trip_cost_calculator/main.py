d00838335d8c0a38a8d0f4f24d6c3ae0fc3ed601
JS SHREYAS <shreyas12js@gmail.com>
2026-04-13 22:02:22 +0530

    Update main.py

--- 05_trip_cost_calculator/main.py
+++ 05_trip_cost_calculator/main.py
@@ -1,1 +1,14 @@
+print("Welcome to the Trip Cost Calculator!")
 
+days = int(input("How many days will you stay? "))
+hotel = float(input("How much does hotel cost per night? $"))
+flight = float(input("How much does flight cost? $"))
+car = float(input("If you need rental car please enter the price otherwise enter zero. $"))
+other = float(input("Enter other possible expenses $"))
+
+hotel_total = hotel * days
+car_total = car * days
+
+total_cost = round(hotel_total + flight + car_total + other , 2)
+
+print(f"Total Cost: ${total_cost}")
