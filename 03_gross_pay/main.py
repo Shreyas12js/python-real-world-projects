20b1db9e0bc21b8327364f8eb75623255b8dcc6c
JS SHREYAS <shreyas12js@gmail.com>
2026-04-13 20:14:42 +0530

    Update main.py

--- 03_gross_pay/main.py
+++ 03_gross_pay/main.py
@@ -1,6 +1,6 @@
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate: "))
 
-pay = round(hours * rate , 2)
+pay = round(hours * rate , 2)   # round function used
 
 print(f"Pay: {pay} ")
