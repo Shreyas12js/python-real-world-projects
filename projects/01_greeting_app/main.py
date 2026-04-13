3427876a3f14e7f12200a2715ecbd2145374fa2a
JS SHREYAS <shreyas12js@gmail.com>
2026-04-13 17:19:21 +0530

    Update main.py

--- projects/01_greeting_app/main.py
+++ projects/01_greeting_app/main.py
@@ -1,1 +1,25 @@
+# Greeting Project
 
+## Instruction
+
+Write a program that uses input to prompt a user for their name and then welcomes them.
+
+## Input
+
+```
+Shreyas
+```
+
+## Output
+
+```
+Hello Shreyas.
+Welcome to Python.
+```
+## Solution
+
+https://github.com/Shreyas12js/python-real-world-projects/blob/main/01_beginner/greeting_app/main.py
+
+## Output Screenshot
+
+![Output](sample_output.png)
